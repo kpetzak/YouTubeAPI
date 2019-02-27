154660154af0475477a846476b288f08142a8828
kpetzak <47672796+kpetzak@users.noreply.github.com>
2019-02-27 15:19:28 -0500

    First try at the Youtube API

---
+++ YouTubeAPI/Search.cs
@@ -0,0 +1,69 @@
+﻿using Google.Apis.Services;
+using Google.Apis.YouTube.v3;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YouTubeAPI
+{
+    /// <summary>
+    /// YouTube Data API v3 sample: search by keyword.
+    /// Relies on the Google APIs Client Library for .NET, v1.7.0 or higher.
+    /// See https://developers.google.com/api-client-library/dotnet/get_started
+    ///
+    /// Set ApiKey to the API key value from the APIs & auth > Registered apps tab of
+    ///   https://cloud.google.com/console
+    /// Please ensure that you have enabled the YouTube Data API for your project.
+    /// https://developers.google.com/youtube/v3/code_samples/dotnet#retrieve_my_uploads
+    /// </summary>
+    internal class Search
+    {
+        [STAThread]
+
+        internal async Task Run()
+        {
+            var youtubeService = new YouTubeService(new BaseClientService.Initializer()
+            {
+                ApiKey = "REPLACE_ME",
+                ApplicationName = this.GetType().ToString()
+            });
+
+            var searchListRequest = youtubeService.Search.List("snippet");
+            searchListRequest.Q = "Google"; // Replace with your search term.
+            searchListRequest.MaxResults = 50;
+
+            // Call the search.list method to retrieve results matching the specified query term.
+            var searchListResponse = await searchListRequest.ExecuteAsync();
+
+            List<string> videos = new List<string>();
+            List<string> channels = new List<string>();
+            List<string> playlists = new List<string>();
+
+            // Add each result to the appropriate list, and then display the lists of
+            // matching videos, channels, and playlists.
+            foreach (var searchResult in searchListResponse.Items)
+            {
+                switch (searchResult.Id.Kind)
+                {
+                    case "youtube#video":
+                        videos.Add(String.Format("{0} ({1})", searchResult.Snippet.Title, searchResult.Id.VideoId));
+                        break;
+
+                    case "youtube#channel":
+                        channels.Add(String.Format("{0} ({1})", searchResult.Snippet.Title, searchResult.Id.ChannelId));
+                        break;
+
+                    case "youtube#playlist":
+                        playlists.Add(String.Format("{0} ({1})", searchResult.Snippet.Title, searchResult.Id.PlaylistId));
+                        break;
+                }
+            }
+
+            Console.WriteLine(String.Format("Videos:\n{0}\n", string.Join("\n", videos)));
+            Console.WriteLine(String.Format("Channels:\n{0}\n", string.Join("\n", channels)));
+            Console.WriteLine(String.Format("Playlists:\n{0}\n", string.Join("\n", playlists)));
+        }
+    }
+}
